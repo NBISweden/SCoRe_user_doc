fdd5b2d5758760792474df3cadd00d1e9670d80e
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-22 10:43:12 +0200

    Index page shows all logos

---
+++ docs/index_1.md
@@ -0,0 +1,10 @@
+# Welcome to the SCoRe user documentation
+
+This site intends to help a user find
+[the resources for his/her needs](resources.md).
+
+[Find the resources for you needs](resources.md).
+
+---
+
+<!-- index_2.md is machine-generated and pasted below this file, index_1.md -->
--- scripts/create_index_page.sh
+++ scripts/create_index_page.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-# Create the page compute.md,
-# by stitching together compute_1.md and compute_2.md
+# Create the page index.md,
+# by stitching together index_1.md and index_2.md
 #
 # Usage:
 #
-#   ./scripts/create_compute_page.sh
+#   ./scripts/create_index_page.sh
 
 if [[ "$PWD" =~ scripts$ ]]; then
     echo "FATAL ERROR."
@@ -14,10 +14,10 @@ if [[ "$PWD" =~ scripts$ ]]; then
     echo " "
     echo "Tip: like this"
     echo " "
-    echo "  ./scripts/create_compute_page.sh"
+    echo "  ./scripts/create_index_page.sh"
     echo " "
     exit 42
 fi
 
-cat docs/compute_1.md docs/compute_2.md > docs/compute.md
+cat docs/index_1.md docs/index_2.md > docs/index.md
 
