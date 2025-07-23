550704e28ae0396a3d26511286bf79ccd8618e72
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-23 10:42:51 +0200

    Fix update

---
+++ scripts/create_other_page.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Create the page other.md,
+# by stitching together other_1.md and other_2.md
+#
+# Usage:
+#
+#   ./scripts/create_other_page.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/create_other_page.sh"
+    echo " "
+    exit 42
+fi
+
+cat docs/other_1.md docs/other_2.md > docs/other.md
+
