0aadfe19a77e8cca587e7de2c0cbccf732528503
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-11-17 11:04:08 +0100

    Remove use of warner

--- scripts/create_courses_page.sh
+++ scripts/create_courses_page.sh
@@ -19,12 +19,4 @@ if [[ "$PWD" =~ scripts$ ]]; then
     exit 42
 fi
 
-
-cd scripts/warner
-cargo build --release
-cd -
-cp scripts/warner/target/release/warner docs/warner
-cd docs
 cat courses_1.md courses_2.md > courses.md
-./warner ./courses.md
-rm ./warner
